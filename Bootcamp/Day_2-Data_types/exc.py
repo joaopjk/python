9f5579ee824f344f168647be5d37efe546132464
joaopjk <joaopjk48@gmail.com>
2023-02-19 17:05:42 -0300

    primitive data types

---
+++ Bootcamp/Day_2-Data_types/exc.py
@@ -0,0 +1,8 @@
+two_digit_number = input("Type a two digit number:")
+if len(two_digit_number) >= 2:
+    first_digit = two_digit_number[0]
+    second_digit = two_digit_number[1]
+    if first_digit.isdigit() and second_digit.isdigit():
+        print(int(first_digit) + int(second_digit))
+    else:
+        print("One of this values not a int")
---
+++ Bootcamp/Day_2-Data_types/primitive_data_types.py
@@ -0,0 +1,26 @@
+# Strings
+print("Hello"[0])
+
+# Integer
+print(123 + 345)
+print(342_653_000)
+
+# Float
+print(3.1233)
+
+# Boolean
+print(True)
+print(False)
+
+# num_char = len(input("What is you name?"))
+# # print("Your name has " + num_char + " characters") Error
+# print("Your name has " + str(num_char) + " characters")
+# print(num_char)
+# print(type(num_char))
+a = str(True)
+print(type(a))
+
+b = float("123.3")
+print(type(b))
+print(b)
+print(b + float("123.5"))
