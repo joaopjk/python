a7a5bffcab606cc47f5acff1f7444e636c25bf14
joaopjk <joaopjk48@gmail.com>
2023-02-19 15:20:09 -0300

    Day 1 - Python

--- Bootcamp/Day_1-Working_with_variables/exc.py
+++ Bootcamp/Day_1-Working_with_variables/exc.py
@@ -6,3 +6,8 @@ print("Day 1 - String Manipulation")
 print("String Concatenation is done with the " + '"+"' + " sign.")
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
+
+print("Welcome to the band name genarator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+print(f"You band name could be {city} {pet}")
